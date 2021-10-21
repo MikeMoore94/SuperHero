b2ebcd0031e6c384a66710a1b2c745a9d155f0e8
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-10-21 15:59:31 -0400

    Added debugging to the project

--- superhero_project/superheros/views.py
+++ superhero_project/superheros/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from .models import Superhero
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'superheros/index.html',)
+    all_heros = Superhero.objects.all()
+    context = {
+        'all_heros': all_heros
+    }
+    return render(request, 'superheros/index.html', context)
