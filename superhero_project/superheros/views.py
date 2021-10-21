f9fc5f0f115baf72f0ba1a8e907f4f46b6da193a
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-10-21 17:08:36 -0400

    added the detail html page

--- superhero_project/superheros/views.py
+++ superhero_project/superheros/views.py
@@ -11,3 +11,10 @@ def index(request):
         'all_heros': all_heros
     }
     return render(request, 'superheros/index.html', context)
+
+def detail(request, hero_id):
+    single_hero = Superhero.objects.get(pk = hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    return render(request, 'superheros/detail.html', context)
